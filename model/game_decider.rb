260572babaa354092bc1a00396b7b5e29c5feb10
Richard Tan <chardos@gmail.com>
2015-11-19 00:35:10 +1100

    Animate bg color and circle

--- model/game_decider.rb
+++ model/game_decider.rb
@@ -2,15 +2,11 @@ class GameDecider
   class << self
     def result(player_action, computer_action)
 
-      win = 'You<br>win'
-      lose = 'You<br>lose'
-      draw = 'Draw<br>game'
-
-      return draw if player_action == computer_action
-      return win if player_action == 'rock' && computer_action == 'scissors'
-      return win if player_action == 'scissors' && computer_action == 'paper'
-      return win if player_action == 'paper' && computer_action == 'rock'
-      lose
+      return 'draw' if player_action == computer_action
+      return 'win' if player_action == 'rock' && computer_action == 'scissors'
+      return 'win' if player_action == 'scissors' && computer_action == 'paper'
+      return 'win' if player_action == 'paper' && computer_action == 'rock'
+      'lose'
     end
   end
 end
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,9 @@
 $('.circle').css('width', $('.circle').outerHeight() );
 
 //animate hands
-$('body').addClass('animate-hands')
+$('body').addClass('animate-hands');
+
+//aniate circle
+setTimeout(function(){
+  $('.circle').addClass('in');
+},200)
