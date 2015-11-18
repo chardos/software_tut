145ae8a5aec5ccbe4518656620980b81d24e6802
Richard Tan <chardos@gmail.com>
2015-11-18 23:58:28 +1100

    Hands animating in

--- model/game_decider.rb
+++ model/game_decider.rb
@@ -1,11 +1,16 @@
 class GameDecider
   class << self
     def result(player_action, computer_action)
-      return 'Draw' if player_action == computer_action
-      return 'Win' if player_action == 'rock' && computer_action == 'scissors'
-      return 'Win' if player_action == 'scissors' && computer_action == 'paper'
-      return 'Win' if player_action == 'paper' && computer_action == 'rock'
-      'Loss'
+
+      win = 'You<br>win'
+      lose = 'You<br>lose'
+      draw = 'Draw<br>game'
+
+      return draw if player_action == computer_action
+      return win if player_action == 'rock' && computer_action == 'scissors'
+      return win if player_action == 'scissors' && computer_action == 'paper'
+      return win if player_action == 'paper' && computer_action == 'rock'
+      lose
     end
   end
 end
---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+$('.circle').css('width', $('.circle').outerHeight() );
+
+//animate hands
+$('body').addClass('animate-hands')
